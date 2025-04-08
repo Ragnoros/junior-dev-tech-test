cc0fc27f3d904348bd678bda18ece1b4ce4e5288
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-04-08 15:57:38 +0100

    added width and height to an alt colour in the json file. added a table in to order original price vs now price and calculated the % decrease

--- CHAGELOG.md
+++ CHAGELOG.md
@@ -6,3 +6,5 @@ Added closing quotes and a comma to the end of line 60 in product.json
 Added closing quotes on line 61 just after "width" in product.json
 
 Changed line 9 inside the quotes in product.vue to "../components/data/product.json"
+
+Added height and width to line 49 and 50 in product.json. don't know if i was supposed to but found it off how it didnt have dimensions like other images.
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,17 +2,42 @@
   <div id="product">
     <!-- Add your code here -->
     <div id="img-panel" class="border text-start" style="width: 65%"></div>
-    <div
-      id="info-panel"
-      class="text-end ms-auto border-bottom"
-      style="width: 30%"
-    >
+    <div id="info-panel" class="text-end ms-auto pe-5" style="width: 30%">
       <p class="text-start ps-1" style="border-left: 5px solid red">
         {{ product.product_offer_label }}
       </p>
-      <p class="text-start">
+      <p class="text-start border-bottom pb-3">
         {{ product.product_title }} | {{ product.product_colour }}
       </p>
+      <div
+        class="d-flex justify-content-between align-items-center border-bottom pb-1"
+      >
+        <table
+          class="table table-sm table-borderless text-start"
+          style="width: 10%"
+        >
+          <thead>
+            <tr>
+              <th scope="col">Original</th>
+              <th scope="col">Now</th>
+            </tr>
+          </thead>
+          <tbody class="text-start p-1">
+            <tr>
+              <td class="text-decoration-line-through">£{{ product.rrp }}</td>
+              <td class="text-danger">£{{ product.selling_price }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p
+          class="text-danger px-2"
+          style="border-left: 2px solid red; border-right: 2px solid red"
+        >
+          Save
+          {{ ((product.rrp - product.selling_price) / product.rrp) * 100 }}%
+        </p>
+      </div>
+      <div id="alt-imgs">{{ product.alternative_colours }}</div>
     </div>
   </div>
 </template>
@@ -38,4 +63,8 @@ export default {
 <style scoped lang="scss">
 @import "../assets/css/poppins.css";
 // Styling to be added here if needed. SASS is allowed if preferred
+th,
+td {
+  font-weight: normal;
+}
 </style>
